7cce54f4710550adacfd79c698d52d6592f98912
vinay sai pavan <banduchodevinaysaipavan@gmail.com>
2025-11-01 15:19:01 +0530

    Updated backend

--- Backend/server.js
+++ Backend/server.js
@@ -25,6 +25,10 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
+app.get("/",(req,res)=>{
+  res.send("Working")
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
